b3b9656daa4f988bda09c52566c75dce80e7f29f
techmeg <42649602+techmeg@users.noreply.github.com>
2021-10-13 17:42:01 -0400

    calculate driver and kid splits

--- src/pages/Driver.js
+++ src/pages/Driver.js
@@ -1,6 +1,90 @@
+import Button from '../components/Button/Button'
+import {useState} from 'react';
+
 function Driver() {
+    const [checkData, setCheckData] = useState({total: '', bar: '', diners: '', tip: ''})
+    const [splitAmounts, setSplitAmounts] = useState({driverAmount: 0, drinkerAmount: 0, tip: 0, totalPlusTip: 0});
+    let totalPlusTip = 0;
+    const result = `Total with ${splitAmounts.tip}% tip is $${splitAmounts.totalPlusTip.toFixed(0)}. The designated driver owes: $${splitAmounts.driverAmount.toFixed(0)}. The remaining diners owe $${splitAmounts.drinkerAmount.toFixed(0)}`
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setCheckData(prevCheckData => {
+            return {
+                ...prevCheckData,
+                [name]: value
+            }
+        })
+    }
+    function calculateSplit(event) {
+        event.preventDefault()
+        const {total, bar, diners, tip} = checkData;
+        const totalFood = total-bar ;
+        const driverAmount = (totalFood/diners) * (tip/100 + 1);
+        totalPlusTip = total * (tip/100 + 1);
+        const drinkerAmount = (totalPlusTip-driverAmount) / (diners-1);
+
+        setSplitAmounts(prevSplit => {
+            return {
+                driverAmount: prevSplit.driverAmount + driverAmount,
+                drinkerAmount: prevSplit.drinkerAmount + drinkerAmount,
+                tip: tip,
+                totalPlusTip: totalPlusTip
+            }
+        })
+            
+        resetData()
+        return splitAmounts;
+    }
+
+    function resultMessage(){
+        if(splitAmounts.drinkerAmount) {
+            return result
+        }
+    }
+
+    function resetData(){
+        setCheckData({total: '', bar: '', diners: '', tip: ''})
+        
+        // console.log(checkData)
+        return checkData
+        }
+    
     return (
-    <div>Driver</div>
-    )
+        <div>
+            <h1>Designated Driver</h1>
+            <p>Enter the Total Check amount, the bar bill (if not separated on the check -- estimate!), the number of diners (including driver) and desired tip percentage.</p>
+            
+            <form onSubmit={calculateSplit} className="check">
+            <div className="form_group">
+                <label >Total Check
+                    <input type="number" name="total" onChange={handleChange} value={checkData.total}/>
+                </label>
+                </div>
+                <div className="form_group">
+                <label >Bar Amount
+                    <input type="number" name="bar" onChange={handleChange} value={checkData.bar}/>
+                </label>
+                </div>
+                <div className="form_group">
+                <label >Number of Diners
+                    <input type="number" name="diners" onChange={handleChange} value={checkData.diners} />
+                </label>
+                </div>
+                <div className="form_group">
+                <label >Tip %
+                    <input type="number" name="tip" onChange={handleChange} value={checkData.tip} />
+                </label>
+                </div>
+                <div id="calculate">
+                    <Button onClick={calculateSplit}value="Calculate Split"/>
+                </div>
+            </form>
+            <div>{resultMessage()}</div>
+            
+               
+        </div>
+        )
+
 }
 export default Driver
--- src/pages/Kids.js
+++ src/pages/Kids.js
@@ -1,6 +1,98 @@
+import Button from '../components/Button/Button'
+import {useState} from 'react';
+
 function Kids() {
+
+    const [checkData, setCheckData] = useState({total: '', kids: '',  adults: '', tip: ''})
+    const [splitAmounts, setSplitAmounts] = useState({kidshare: 0, adultshare: 0, tip: 0, totalPlusTip: 0});
+    let totalPlusTip = 0;
+    const result = `Total with ${splitAmounts.tip}% tip is $${splitAmounts.totalPlusTip.toFixed(0)}. A kid share is: $${splitAmounts.kidshare.toFixed(0)}. The adults each owe $${splitAmounts.adultshare.toFixed(0)}`
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setCheckData(prevCheckData => {
+            return {
+                ...prevCheckData,
+                [name]: value
+            }
+        })
+    }
+    function calculateSplit(event) {
+        event.preventDefault()
+        let {total, kids, adults, tip} = checkData;
+        if(tip === ''){
+            tip = 0;
+        }
+        console.log(total, kids, adults, tip)
+        totalPlusTip = total * (tip/100 + 1);
+        const numberShares = parseInt(adults*2) + parseInt(kids);
+        const kidshare = totalPlusTip/numberShares
+        const adultshare = kidshare*2;
+
+        setSplitAmounts(prevSplit => {
+
+            return {
+                kidshare: prevSplit.kidshare + kidshare,
+                adultshare: prevSplit.adultshare + adultshare,
+                tip: tip,
+                totalPlusTip: totalPlusTip
+            }
+        })
+            
+        resetData()
+        return splitAmounts;
+    }
+
+    function resultMessage(){
+        if(!splitAmounts.adultshare) {
+            return `Enter numbers in all fields`
+        } else {
+            return result
+        }
+    }
+
+    function resetData(){
+        setCheckData({total: '', kids: '', adults: '', tip: ''})
+        
+        // console.log(checkData)
+        return checkData
+        }
+    
     return (
-    <div>Kids Pay Half</div>
-    )
+        <div>
+            <h1>Kids Pay Half</h1>
+            <p>Enter the Total Check amount, the number of kids and adults and desired tip percentage.</p>
+            
+            <form onSubmit={calculateSplit} className="check">
+            <div className="form_group">
+                <label >Total Check
+                    <input type="number" name="total" onChange={handleChange} value={checkData.total} required/>
+                </label>
+                </div>
+                <div className="form_group">
+                <label >Number of Kids
+                    <input type="number" name="kids" onChange={handleChange} value={checkData.kids} min="1" required/>
+                </label>
+                </div>
+                <div className="form_group">
+                <label >Number of Adults
+                    <input type="number" name="adults" onChange={handleChange} value={checkData.adults} min="1" required />
+                </label>
+                </div>
+                <div className="form_group">
+                <label >Tip %
+                    <input type="number" name="tip" onChange={handleChange} value={checkData.tip} />
+                </label>
+                </div>
+                <div id="calculate">
+                    <Button onClick={calculateSplit}value="Calculate Split"/>
+                </div>
+            </form>
+            <div>{resultMessage()}</div>
+            
+               
+        </div>
+        )
+
 }
 export default Kids
